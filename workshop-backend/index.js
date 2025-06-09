5b2227a909660c6f2fd935f9ea5ab1b4c1238aac
Radhey Mohan <radheychoudhary000@gmail.com>
2025-06-09 17:26:08 +0530

    fix index.js

--- workshop-backend/index.js
+++ workshop-backend/index.js
@@ -12,7 +12,8 @@ const { db } = require("./firebaseAdmin");
 const generateCertificate = require("./certificateTemplate");
 
 const app = express();
-const PORT = 5000;
+// const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
