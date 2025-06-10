7fde6c76db874dfecac28b1aa6c78d2106012a1c
Radhey Mohan <radheychoudhary000@gmail.com>
2025-06-11 01:13:18 +0530

    service account key update

--- workshop-backend/stringifyKey.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-const key = require('./serviceAccountKey.json');
-const stringified = JSON.stringify(key);
-
-fs.writeFileSync('serviceAccountKey.env.txt', stringified);
-console.log('✅ Key has been stringified and saved to serviceAccountKey.env.txt');
